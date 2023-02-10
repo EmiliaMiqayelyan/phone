3a6c708b7b7cd45421ebca09f43bfd08cb1e93b0
EmiliaMiqayelyan <emilik200915@gmail.com>
2023-02-10 18:50:06 +0400

    add phone ui

---
+++ src/phone/Phone.js
@@ -0,0 +1,15 @@
+import "./phone.css"
+function  Phone(){
+    return(
+        <div className="phone">
+            <div className="loudspeaker"></div>
+            <div className="camera">
+                <div className="main-camera">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default  Phone;
